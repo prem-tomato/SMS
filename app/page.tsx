7fecd3e845820917c4d8f7d9685a0a2a9a0f8844
prem.tomato12 <prem.tomato12@gmail.com>
2025-07-22 11:32:27 +0530

    fix main page route

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
+import { getAccessToken } from "@/lib/auth";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
+  const accessToken = getAccessToken();
 
   useEffect(() => {
-    router.push("/dashboard");
+    if (accessToken) {
+      router.push("/dashboard");
+    } else {
+      router.push("auth/login");
+    }
   }, [router]);
 
   return null;
