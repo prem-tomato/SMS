96fa20c5b67e1cb5a8990b0512db887fdd5d2a33
prem.tomato12 <prem.tomato12@gmail.com>
2025-07-26 14:12:03 +0530

    all total penalties

--- app/api/final-balance/final-balance.model.ts
+++ app/api/final-balance/final-balance.model.ts
@@ -10,6 +10,8 @@ export const getFinalBalance = async (
         SELECT
           s.name AS society_name,
           s.opening_balance AS society_balance,
+            COALESCE(maintenance_data.total_penalties, 0) AS total_penalties,
+
           COALESCE(expense_data.total_expense, 0) AS total_expense,
           COALESCE(maintenance_data.total_maintenance, 0) AS total_maintenance,
           (
@@ -25,10 +27,15 @@ export const getFinalBalance = async (
           GROUP BY society_id
         ) AS expense_data ON expense_data.society_id = s.id
         LEFT JOIN (
-          SELECT society_id, SUM(current_maintenance)::int AS total_maintenance
-          FROM flats
-          GROUP BY society_id
-        ) AS maintenance_data ON maintenance_data.society_id = s.id
+  SELECT
+    f.society_id,
+    SUM(f.current_maintenance)::int + COALESCE(SUM(fp.amount), 0)::int AS total_maintenance,
+    COALESCE(SUM(fp.amount), 0)::int AS total_penalties
+  FROM flats f
+  LEFT JOIN flat_penalties fp ON fp.flat_id = f.id
+  GROUP BY f.society_id
+) AS maintenance_data ON maintenance_data.society_id = s.id
+
         WHERE s.id = $1
       `;
   
