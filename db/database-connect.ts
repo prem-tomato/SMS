71cb7bbb0c445533abff3a1cb063d95466f4f097
prem.tomato12 <prem.tomato12@gmail.com>
2025-07-29 17:24:10 +0530

    fix db connection

--- db/database-connect.ts
+++ db/database-connect.ts
@@ -4,11 +4,10 @@ import Logger from "./configs/logger";
 const databaseLogger = new Logger('database-connect');
 
 const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false,
+  },
 });
 
 export default pool;
